556eaabe506408e8d812978dcf6ba3cb04a91fa6
NJ <nj.namju@gmail.com>
2019-10-02 08:54:28 -0700

    init sandbox

---
+++ src/components/SandBox.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { App } from '../lib/App';
+export class SandBox extends Component {
+  app: App;
+
+  componentDidMount() {
+    this.app = new App();
+  }
+
+  public render() {
+    return (
+      <div
+        id="SandBox"
+        style={{ height: '100%', width: '100%', position: 'absolute' }}
+      >
+        React Typescript SandBox Starter!
+      </div>
+    );
+  }
+}
